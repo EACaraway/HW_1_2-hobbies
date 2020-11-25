42415ffa36ecee584a01ebb1784a8821e2c97dbf
Evan Caraway <evan.caraway@outlook.com>
2020-11-24 22:26:30 -0800

    project files

---
+++ HomeWorkWeek1Problem2 hobbies/HomeWorkWeek1Problem2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkWeek1Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Evan Caraway's Hobbies
+            Console.WriteLine("Swimming \nWaterpolo \nStand Up Paddle Boarding \nHiking \nRock Climbing");
+        }
+    }
+}
